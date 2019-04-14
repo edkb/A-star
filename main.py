00348925608076c24019ae8493b405581e7cfe01
edkb <eduk_binoto@hotmail.com>
2019-04-14 17:48:37 -0300

    Added simple heuristics support. Considerable code refactoring. Working for cost and wrong number mode.

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ import copy
 
 class Node:
 
-    def __init__(self, state, cost=0):
+    def __init__(self, state, cost=0, father=None):
 
         self.state = state
         self.cost = cost
         self.sons = []
+        self.father = father
 
     def __str__(self):
         str_repr = '-----\n'
@@ -24,10 +25,29 @@ class Node:
         str_repr += '-----\n'
         return str_repr
 
-    def get_node_sons(self):
-        current_node = self.state
+    @property
+    def f1(self):
+        return self.cost + self.get_simple_heuristics()
 
-        for line_number, line in enumerate(current_node):
+    def get_simple_heuristics(self):
+
+        wrong_numbers = 0
+
+        for line_number, line in enumerate(self.state):
+            for column_number, number in enumerate(line):
+
+                correct_number = final_state[line_number][column_number]
+
+                if number != correct_number:
+                    wrong_numbers += 1
+
+        return wrong_numbers
+
+    def get_node_sons(self):
+
+        sons = []
+
+        for line_number, line in enumerate(self.state):
             for column_number, number in enumerate(line):
 
                 # If is the square
@@ -35,10 +55,10 @@ class Node:
 
                     # Square can go up
                     if line_number > 0:
-                        new_son = copy.deepcopy(current_node)
+                        new_son = copy.deepcopy(self.state)
 
                         # Get the number of the up position
-                        flip_number = current_node[line_number - 1][column_number]
+                        flip_number = self.state[line_number - 1][column_number]
 
                         # Set the number of the up position to 0
                         new_son[line_number - 1][column_number] = 0
@@ -46,14 +66,16 @@ class Node:
                         # Set the current 0 position to the flip number
                         new_son[line_number][column_number] = flip_number
 
-                        self.sons.append(Node(new_son, self.cost+1))
+                        sons.append(
+                            Node(state=new_son, cost=self.cost+1, father=self)
+                        )
 
                     # Square can go down
                     if line_number < 2:
-                        new_son = copy.deepcopy(current_node)
+                        new_son = copy.deepcopy(self.state)
 
                         # Get the number of the down position
-                        flip_number = current_node[line_number + 1][column_number]
+                        flip_number = self.state[line_number + 1][column_number]
 
                         # Set the number of the down position to 0
                         new_son[line_number + 1][column_number] = 0
@@ -61,14 +83,16 @@ class Node:
                         # Set the current 0 position to the flip number
                         new_son[line_number][column_number] = flip_number
 
-                        self.sons.append(Node(new_son, self.cost+1))
+                        sons.append(
+                            Node(state=new_son, cost=self.cost+1, father=self)
+                        )
 
                     # Square can go right
                     if column_number < 2:
-                        new_son = copy.deepcopy(current_node)
+                        new_son = copy.deepcopy(self.state)
 
                         # Get the number of the right position
-                        flip_number = current_node[line_number][column_number + 1]
+                        flip_number = self.state[line_number][column_number + 1]
 
                         # Set the number of the right position to 0
                         new_son[line_number][column_number + 1] = 0
@@ -76,14 +100,16 @@ class Node:
                         # Set the current 0 position to the flip number
                         new_son[line_number][column_number] = flip_number
 
-                        self.sons.append(Node(new_son, self.cost+1))
+                        sons.append(
+                            Node(state=new_son, cost=self.cost+1, father=self)
+                        )
 
                     # Square can go left
                     if column_number > 0:
-                        new_son = copy.deepcopy(current_node)
+                        new_son = copy.deepcopy(self.state)
 
                         # Get the number of the left position
-                        flip_number = current_node[line_number][column_number - 1]
+                        flip_number = self.state[line_number][column_number - 1]
 
                         # Set the number of the left position to 0
                         new_son[line_number][column_number - 1] = 0
@@ -91,7 +117,10 @@ class Node:
                         # Set the current 0 position to the flip number
                         new_son[line_number][column_number] = flip_number
 
-                        self.sons.append(Node(new_son, self.cost+1))
+                        sons.append(
+                            Node(state=new_son, cost=self.cost+1, father=self)
+                        )
+        return sons
 
     def print_sons(self):
         for sun_number, son in enumerate(self.sons):
@@ -99,24 +128,28 @@ class Node:
             print(son)
 
 
-def reconstruct_path(came_from, current):
+def build_path(last_node):
+    """
+    Reconstructs the path from the final node up to
+    the initial node by iterating over the nodes father
 
-    total_path = current
+    :param last_node: The final node found by the algorithm
+    :return: List of nodes forming the shortest path
+             from the initial node to the goal
+    """
+    path = []
+    while last_node:
+        path.insert(0, last_node)
+        last_node = last_node.father
+    return path
 
-    while current in came_from.keys():
 
-        current = came_from[current]
-        total_path.append(current)
-
-    return total_path
+def get_cost(current_node):
+    return current_node.cost
 
 
 def get_heuristic(current_node):
-    return 10
-
-
-def distance_between(current_node, son):
-    return 20
+    return current_node.f1
 
 
 final_state = [
@@ -134,36 +167,67 @@ initial_state = [
 ]
 
 
-def uniform_cost_search():
+def a_star(mode='C'):
+    """
+    Main search algorithm
+
+    Finds the best possible path between any
+    initial board state and the final state
+
+    :param mode: Define the sorting strategy
+    :return: list of nodes containing board states
+    """
+
+    # By default we use just the cost
+    # as our sorting strategy
+    f = get_cost
+
+    if mode == 'F1':
+        f = get_heuristic
 
     first_node = Node(initial_state)
-
-    open_nodes = []
-    open_nodes.append(first_node)
-
-    closed_nodes = set()
-
     current_cost = first_node.cost
+
+    # The python data type list is used to store
+    # the open nodes because we need to rely on the
+    # cost and heuristic values to get the most
+    # promising results and lists are easy to store
+    # and sort data in this way
+    open_nodes = [first_node]
+
+    # The set data structure is used because
+    # its a lightweight way to store unordered
+    # and unrepeated peaces of data (our nodes)
+    closed_nodes = set()
 
     while open_nodes:
 
-        open_nodes.sort(key=lambda node: node.cost)
+        # Sorts the open nodes list by node cost
+        # with the lowest cost first
+        open_nodes.sort(key=f)
+
+        # Get the lowest cost node and remove it from
+        # the open nodes list
         current_node = open_nodes.pop(0)
         closed_nodes.add(current_node)
 
         if current_node.state == final_state:
             print('\nWe made it!')
             print(f'Visited nodes: {len(closed_nodes)}')
-            print(f'Path size: {current_node.cost}')
-            break
+            print(f'Path size: {current_node.cost} ')
+
+            return build_path(current_node)
 
         if current_node.cost > current_cost:
+            # Just for debugging
             current_cost = current_node.cost
             print(f'Current cost: {current_cost}')
 
-        current_node.get_node_sons()
-        sons = current_node.sons
+        sons = current_node.get_node_sons()
 
+        # This nested loops checks if the son of the
+        # current node is already in the closed nodes list.
+        # If doesn't, add it to the open nodes list
         for son in sons:
             for visited in closed_nodes:
                 if son.state == visited.state:
@@ -172,50 +236,8 @@ def uniform_cost_search():
                 open_nodes.append(son)
 
 
-def a_star():
-    """
-    Main algorithm
-    :return:
-    """
+if __name__ == '__main__':
 
-    closed_nodes = []
-    open_nodes = []
+    uniform_cost = a_star()
 
-    # Add the starting node to the open nodes list
-    open_nodes.append(initial_state)
-
-    came_from = {}
-
-    path_cost = {'start': 0}
-    total_cost = path_cost['start'] + get_heuristic('start')
-
-    while open_nodes:
-
-        open_nodes.sort()
-        current_node = open_nodes[0]
-
-        if current_node == final_state:
-            return reconstruct_path(came_from, current_node)
-
-        open_nodes.remove(current_node)
-        closed_nodes.append(current_node)
-
-        sons = get_node_sons(current_node)
-
-        for son in sons:
-
-            if son in closed_nodes:
-                continue
-
-            current_score = path_cost[current_node] + distance_between(current_node, son)
-
-            if son not in open_nodes:
-                open_nodes.append(son)
-                open_nodes.sort()
-
-            elif current_score >= path_cost[son]:
-                continue
-
-        came_from[son] = current_node
-        path_cost[son] = current_score
-        total_cost = path_cost[current_node] + distance_between(son, final_state)
+    simple_heuristics = a_star('F1')
